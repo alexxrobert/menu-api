cd96876d3b78bdc2c535c0ce10270e2bb4b7e159
Marx JMoura <marx.jmoura@gmail.com>
2019-09-09 09:05:01 -0300

    Generate migrations to functional indexes

---
+++ src/Storefront.Menu.API/Migrations/20190909115049_CreateFunctionalIndexes.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Storefront.Menu.API.Models.DataModel;
+
+namespace Storefront.Menu.API.Migrations
+{
+    [ExcludeFromCodeCoverage]
+    public partial class CreateFunctionalIndexes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"
+                CREATE INDEX idx_item_group_name
+                ON {ApiDbContext.Schema}.item_group (public.ci_ai(title));
+            ");
+
+            migrationBuilder.Sql($@"
+                CREATE INDEX idx_item_name
+                ON {ApiDbContext.Schema}.item (public.ci_ai(name));
+            ");
+
+            migrationBuilder.Sql($@"
+                CREATE INDEX idx_option_group_name
+                ON {ApiDbContext.Schema}.option_group (public.ci_ai(title));
+            ");
+
+            migrationBuilder.Sql($@"
+                CREATE INDEX idx_option_name
+                ON {ApiDbContext.Schema}.option (public.ci_ai(name));
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP INDEX IF EXISTS idx_item_group_name;");
+            migrationBuilder.Sql(@"DROP INDEX IF EXISTS idx_item_name;");
+            migrationBuilder.Sql(@"DROP INDEX IF EXISTS idx_option_group_name;");
+            migrationBuilder.Sql(@"DROP INDEX IF EXISTS idx_option_name;");
+        }
+    }
+}
