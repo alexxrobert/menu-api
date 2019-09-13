56d1e7f0414350dd1b54634519a9e50ca634f42c
Marx JMoura <marx.jmoura@gmail.com>
2019-09-13 15:27:18 -0300

    Add documentation for item groups

--- src/Storefront.Menu.API/Models/TransferModel/ItemGroups/ItemGroupListQuery.cs
+++ src/Storefront.Menu.API/Models/TransferModel/ItemGroups/ItemGroupListQuery.cs
@@ -4,6 +4,10 @@ namespace Storefront.Menu.API.Models.TransferModel.ItemGroups
 {
     public sealed class ItemGroupListQuery : QueryOffset
     {
+        /// <summary>
+        /// Title of the item group
+        /// </summary>
+        /// <value></value>
         [MaxLength(50)]
         public string Title { get; set; }
     }
