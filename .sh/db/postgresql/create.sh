0b5aabd49c8b75c1fa8efe270f465fbb99b1ad78
Marx JMoura <marx.jmoura@gmail.com>
2019-09-29 19:09:30 -0300

    [skip ci] Update shell script to use Docker container

--- .sh/db/postgresql/create.sh
+++ .sh/db/postgresql/create.sh
@@ -12,6 +12,6 @@ create_database_sql=$(<$sqldir//create_database.sql)
 create_extensions_sql=$(<$sqldir//create_extensions.sql)
 create_functions_sql=$(<$sqldir//create_functions.sql)
 
-psql -h $PGHOST -p $PGPORT -U $PGUSER -c "${create_database_sql//p_dbname/$DBNAME}"
-psql -h $PGHOST -p $PGPORT -U $PGUSER -c "$create_extensions_sql" -d $DBNAME
-psql -h $PGHOST -p $PGPORT -U $PGUSER -c "$create_functions_sql"  -d $DBNAME
+docker exec -it postgres psql -U $PGUSER -c "${create_database_sql//p_dbname/$DBNAME}"
+docker exec -it postgres psql -U $PGUSER -c "$create_extensions_sql" -d $DBNAME
+docker exec -it postgres psql -U $PGUSER -c "$create_functions_sql"  -d $DBNAME
